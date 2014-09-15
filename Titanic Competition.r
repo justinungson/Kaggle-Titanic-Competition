2cb831807b4d099b92987c181509579a94852c99
Justin Ungson <justinungson@gmail.com>
2014-09-15 15:45:55 -0700

    Exploratory data analysis and visualization

--- Titanic Competition.r
+++ Titanic Competition.r
@@ -1,8 +1,18 @@
 ## Download data from Kaggle ##
 
+if(!file.exists("data")){
+  dir.create("data")
+}
+
 train.URL <- "http://www.kaggle.com/c/titanic-gettingStarted/download/train.csv"
+download.file(train.URL, destfile = "./data/train.csv")
+train.date.downloaded <- date()
+
 test.URL <- "http://www.kaggle.com/c/titanic-gettingStarted/download/test.csv"
+download.file(test.URL, destfile = "./data/test.csv")
+test.date.downloaded <- date()
 
-train.data <- read.csv(train.URL, header = TRUE)
-test.data <- read.csv(test.URL, header = TRUE)
-
+train.data <- read.csv("./data/train.csv", header = TRUE, 
+                        stringsAsFactors = FALSE)
+test.data <- read.csv("./data/test.csv", header = TRUE, 
+                        stringsAsFactors = FALSE)
